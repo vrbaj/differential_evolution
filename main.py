5e40c3f261f8d290c402da452a393612269a9bd6
xr72 <vrbik.vrbik@gmail.com>
2021-08-03 16:25:20 +0200

    DE/current-to-best/1 added

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class DifferentialEvolution:
         self.population = []
         self.strategy = strategy
         self.generation = 0
-        self.best_individual = None
-        self.best_individual_idx = None
+        self.generation_best_individual = None
+        self.generation_best_individual_idx = None
         self.best_individual_history = []
 
     def initialize(self):
@@ -31,7 +31,7 @@ class DifferentialEvolution:
         while self.generation < self.max_iterations:
             self.generation += 1
             self.get_best()
-            self.best_individual_history.append(self.best_individual)
+            self.best_individual_history.append(self.generation_best_individual)
             for idx, individual in enumerate(self.population):
                 crossover_candidates = self.generate_crossover_candidates(idx)
                 new_individual = []
@@ -40,6 +40,7 @@ class DifferentialEvolution:
                     if cr > self.crossover:
                         new_individual.append(individual[dimension])
                     else:
+                        # check boundaries, if violation, random generate new one
                         new_individual.append(self.mutate(crossover_candidates, dimension))
                 if self.cost_function(new_individual) < self.cost_function(individual):
                     self.population[idx] = new_individual
@@ -53,20 +54,33 @@ class DifferentialEvolution:
         elif self.strategy == "DE/rand/2":
             return crossover_candidates
         elif self.strategy == "DE/best/1":
-            crossover_candidates[0] = self.best_individual_idx
+            crossover_candidates[0] = self.generation_best_individual_idx
             return crossover_candidates[:3]
+        elif self.strategy == "DE/best/2":
+            crossover_candidates[0] = self.generation_best_individual_idx
+            return crossover_candidates
+        elif self.strategy == "DE/current-to-best/1":
+            crossover_candidates[0] = self.generation_best_individual_idx
+            crossover_candidates[1] = idx
+            return crossover_candidates[:4]
 
     def mutate(self, crossover_candidates, dimension):
         if self.strategy == "DE/rand/1" or self.strategy == "DE/best/1":
             return self.population[crossover_candidates[0]][dimension] + \
                    self.mutation * (self.population[crossover_candidates[1]][dimension] -
                                     self.population[crossover_candidates[2]][dimension])
-        elif self.strategy == "DE/rand/2":
+        elif self.strategy == "DE/rand/2" or self.strategy == "DE/best/2":
             return self.population[crossover_candidates[0]][dimension] + \
                    self.mutation * (self.population[crossover_candidates[1]][dimension] -
                                     self.population[crossover_candidates[2]][dimension]) + \
                    self.mutation * (self.population[crossover_candidates[3]][dimension] -
                                     self.population[crossover_candidates[4]][dimension])
+        elif self.strategy == "DE/current-to-best/1":
+            return self.population[crossover_candidates[1]][dimension] + \
+                   self.mutation[0] * (self.population[crossover_candidates[0]][dimension] -
+                                       self.population[crossover_candidates[1]][dimension]) +\
+                   self.mutation[1] * (self.population[crossover_candidates[2]][dimension] -
+                                       self.population[crossover_candidates[3]][dimension])
 
     def get_best(self):
         best_cost = np.inf
@@ -76,9 +90,13 @@ class DifferentialEvolution:
                 best = individual
                 best_cost = cost
                 best_idx = idx
-        self.best_individual = best
-        self.best_individual_idx = best_idx
+        self.generation_best_individual = best
+        self.generation_best_individual_idx = best_idx
         return best
+
+    def filter_history(self, dimension):
+        solutions = [solution[dimension] for solution in self.best_individual_history]
+        return solutions
 
 
 def function_to_minimize(x):
@@ -87,10 +105,13 @@ def function_to_minimize(x):
 
 if __name__ == '__main__':
     diff_evolution = DifferentialEvolution(function_to_minimize, bounds=[[-5, 5], [-5, 5]], max_iterations=100,
-                                           population_size=50,  mutation=0.5, crossover=0.7, strategy="DE/best/1")
+                                           population_size=50,  mutation=[0.5, 0.7], crossover=0.7,
+                                           strategy="DE/current-to-best/1")
     diff_evolution.initialize()
     diff_evolution.evolve()
     print("the best solution: ", diff_evolution.get_best())
-    print(diff_evolution.best_individual_history)
-    plt.plot(np.log10(diff_evolution.best_individual_history))
+    fig, (ax1, ax2) = plt.subplots(2)
+    fig.suptitle("Error")
+    ax1.plot(np.log10(np.abs(diff_evolution.filter_history(0))))
+    ax2.plot(np.log10(np.abs(diff_evolution.filter_history(1))))
     plt.show()
