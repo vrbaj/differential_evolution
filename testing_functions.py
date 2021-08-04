4aa83015afb505af38ff717494d1086179234857
xr72 <vrbik.vrbik@gmail.com>
2021-08-04 12:56:58 +0200

    Beale function added

--- testing_functions.py
+++ testing_functions.py
@@ -13,9 +13,20 @@ def sphere_function(x):
 def rastrigin_function(x):
     """
     Rastrigin function given as $y(x) = A * n + \sum_{i=1}^n(x_i^2 - A * cos(2 * \pi * x_i))$
+    with global minimum y(0,....0) = 0
     :param x: input vector of length n
     :return: the value of Rastrigin function y(x)
     """
     A = 10
     return A * len(x) + np.sum(np.asarray(x) ** 2 - A * np.cos(2 * np.pi * np.asarray(x)))
 
+
+def beale_function(x):
+    """
+    Beale function given as $f(x,y) = (1.5 - x - xy)^2 + (2.25 - x + xy^2)^2 + (2.625 - x + xy^3)^2$
+    with global minimum f(3, 0.5) = 0
+    :param x: input vector [x, y]
+    :return: the value of Beale function f(x,y)
+    """
+    return (1.5 - x[0] + x[0] * x[1]) ** 2 + (2.25 - x[0] + x[0] * x[1] ** 2) ** 2 + \
+           (2.625 - x[0] + x[0] * x[1] ** 3 ) ** 2
